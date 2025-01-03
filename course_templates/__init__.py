5a6991136798ae1ec59bd9ee9de937cffcfddce0
awais qureshi <awais.qureshi@arbisoft.com>
2025-01-03 14:21:46 +0500

    first commit

---
+++ course_templates/__init__.py
@@ -0,0 +1,5 @@
+"""
+Use for creating custom course templates..
+"""
+
+__version__ = '0.1.0'
---
+++ tests/test_course_templates.py
@@ -0,0 +1,12 @@
+"""
+Tests for course_templates.py.
+"""
+
+import pytest
+
+
+@pytest.mark.skip(reason="Placeholder to allow pytest to succeed before real tests are in place.")
+def test_placeholder():
+    """
+    TODO: Delete this test once there are real tests.
+    """
